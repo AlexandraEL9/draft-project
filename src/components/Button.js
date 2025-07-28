ab779e24b023a242e2a6476bf27fe59b7083bf7d
AlexandraEL9 <alexandraelee9@gmail.com>
2025-07-28 14:57:33 +0100

    add basic button component to app

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/Button.css';
+
+function Button() {
+  return (
+    <button className="custom-button">Start Routine</button>
+  );
+}
+
+export default Button;
